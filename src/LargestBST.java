4ac10ab7f82280b6758b272f3ac5366509ff8f0f
minkal-chaudhary <61204357+minkal-chaudhary@users.noreply.github.com>
2021-01-28 23:40:47 +0530

    initialised Git

---
+++ src/LargestBST.java
@@ -0,0 +1,10 @@
+
+public class LargestBST {
+	Node root;
+public static void main(String [] args)
+{
+	LargestBST tree=new LargestBST();
+	tree.root=new Node(50);
+	tree.root.left=new Node(10);
+}
+}
---
+++ src/Node.java
@@ -0,0 +1,11 @@
+
+public class Node {
+int data;
+Node left,right;
+public Node(int data) {
+	super();
+	this.data = data;
+	
+}
+
+}
