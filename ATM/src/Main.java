9f13a43bc982184be45e129799a64eea9432537a
minkal-chaudhary <61204357+minkal-chaudhary@users.noreply.github.com>
2021-01-29 13:59:44 +0530

    veg in main

--- ATM/src/Main.java
+++ ATM/src/Main.java
@@ -13,8 +13,7 @@ class Main {
    al.add(200);
    al.add(100);
    Denomination d=new Denomination(al);
-   
-<<<<<<< Updated upstream
+ 
  ATM atm1=new ATM(d);
    
    atm1.topUp(new Amount(23900));
@@ -23,15 +22,14 @@ class Main {
    atm1.transit(new Transaction(50201,"Deposit"));
    System.out.println("main branch ");
    System.out.println(atm1);
-=======
  ATM atm=new ATM(d);
- ATM atm1=new ATM(d);
+ //ATM atm1=new ATM(d);
    atm.topUp(new Amount(23900));
    System.out.println(atm);
    atm.transit(new Transaction(10400,"Withdraw"));
    atm.transit(new Transaction(50201,"Deposit"));
    System.out.println("Branch ATM Changes again");
    System.out.println(atm);
->>>>>>> Stashed changes
+
 	}
 }
